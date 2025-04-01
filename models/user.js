d78a74cf66634a0bf980e12a296ef8bb86527025
LenoMai <lenamai4002@gmail.com>
2025-03-31 21:51:27 -0400

    first commit

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName: {type: String, required: [true, 'First name is required']},
+    lastName: {type: String, required: [true, 'Last name is required']},
+    email: {type: String, required: [true, 'Email is required'],unique
+},
+{timestamps: true}
+);
+module.exports = mongoose.model('User', userSchema);
